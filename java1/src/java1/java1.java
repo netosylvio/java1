e4125a79ab98c7566df4d7c89b8b8f5e5b3ec0b3
Cliente <Cliente@192.168.100.2>
2022-04-15 18:44:01 -0300

    Incluido media

--- java1/src/java1/java1.java
+++ java1/src/java1/java1.java
@@ -5,7 +5,11 @@ public class java1 {
 	public static void main(String[] args) {
 	
 		
-		System.out.println("tste");
+		double nota1 = 10;
+		double nota2 = 32;
+		double media = (nota1+nota2)/2;
+		
+		System.out.println("media = "+media);
 		
 	} 
 		
