e2f09e3961872ac3eddba7177c14f7b6d17f2b64
Cliente <Cliente@192.168.100.2>
2022-04-16 00:36:30 -0300

    operador ternario

--- java1/src/java1/java1.java
+++ java1/src/java1/java1.java
@@ -3,13 +3,15 @@ package java1;
 public class java1 {
 	
 	public static void main(String[] args) {
-	
 		
 		double nota1 = 10;
 		double nota2 = 32;
 		double media = (nota1+nota2)/2;
 		
 		System.out.println("media = "+media);
+		
+		String resultado = media>= 70 ? "aprovado" : "Reprovado";
+		System.out.println("Resultado = "+resultado); 
 		
 	} 
 		
