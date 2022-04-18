ef25e740703755934fabe58977c7756c4b8a9f5f
Cliente <Cliente@192.168.100.3>
2022-04-18 15:04:20 -0300

    JoptionPane

--- java1/src/java1/java1.java
+++ java1/src/java1/java1.java
@@ -1,8 +1,37 @@
 package java1;
 
+import javax.swing.JOptionPane;
 public class java1 {
 	
 	public static void main(String[] args) {
+		
+		/* caixa de dialogo */
+		String carros = JOptionPane.showInputDialog("Informe a qtd de carros");
+		String pessoas = JOptionPane.showInputDialog("Informe a qtd de pessoas");
+		
+		/* convertendo de String para Double */ 
+		double carroNumero = Double.parseDouble(carros);
+		double pessoasNumero = Double.parseDouble(pessoas);
+		
+		/* convertendo double para inteiro */ 
+		int divisao = (int) (carroNumero / pessoasNumero);
+		
+		double resto = carroNumero % pessoasNumero; 
+		
+		/*caixa de confirmação */
+		int resposta = JOptionPane.showConfirmDialog(null, "Deseja ver o resultado da divisao?");
+		
+		if (resposta == 0 ) {
+			JOptionPane.showMessageDialog(null, "Divisao de pessoas deu "+ divisao);		
+		}
+		
+		resposta = JOptionPane.showConfirmDialog(null, "Deseja ver o resultado do resto?");
+		if (resposta == 0 ) {
+			JOptionPane.showMessageDialog(null, "Resto deu "+ resto);		
+		}
+		
+		
+		JOptionPane.showMessageDialog(null, "Divisao de pessoas deu "+ divisao + " carros e sobrou  " + resto + " carros");
 		
 		double nota1 = 10;
 		double nota2 = 32;
@@ -12,6 +41,16 @@ public class java1 {
 		
 		String resultado = media>= 70 ? "aprovado" : "Reprovado";
 		System.out.println("Resultado = "+resultado); 
+		
+		if (media >=20) {
+			if (media >=25) {
+				System.out.println("aprovado");	
+			} else {
+				System.out.println("Resultado = Recuperaçao");
+				
+			}
+		}
+		
 		
 	} 
 		
